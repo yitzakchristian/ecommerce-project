4cde4052677a8aa306df9c5396e416e5141f4bf7
Christian Yitzak <christian.yitzak@gmail.com>
2026-03-19 19:55:32 +0700

    move tracking page into react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router'
 import { HomePage } from './pages/HomePage'
 import { CheckoutPage } from './pages/checkout/CheckoutPage'
 import { OrdersPage } from './pages/OrdersPage'
+import { TrackingPage } from './pages/TrackingPage'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route index element={<HomePage />} />
       <Route path='checkout' element={<CheckoutPage />} />
       <Route path='orders' element={<OrdersPage />} />
+      <Route path='tracking' element={<TrackingPage />} />
     </Routes>
   )
 }
