3660e8807b48c2b6af2bb74aeccd2386967755f0
Christian Yitzak <christian.yitzak@gmail.com>
2026-03-20 08:41:37 +0700

    use axios to fetch the data

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,11 @@
+import axios from 'axios'
 import { Header } from '../components/Header'
 import { products } from '../../../starting-code/data/products'
 import './HomePage.css'
 
 export function HomePage() {
-  fetch('http://localhost:3000/api/products')
-    .then(response => response.json())
-    .then(data => console.log(data))
+  axios.get('http://localhost:3000/api/products')
+    .then(response => console.log(response.data))
 
   return (
     <>
