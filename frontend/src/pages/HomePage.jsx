17bb945680398967bcc3b33e85dfe85d17df74b5
Christian Yitzak <christian.yitzak@gmail.com>
2026-03-20 08:53:20 +0700

    add server proxy config for /api and /images

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -9,11 +9,11 @@ export function HomePage() {
 
   useEffect(() => {
     axios
-      .get('http://localhost:3000/api/products')
+      .get('/api/products')
       .then(response => setProducts(response.data))
 
     axios
-      .get('http://localhost:3000/api/cart-items')
+      .get('/api/cart-items')
       .then(response => setCart(response.data))
   }, []);
 
